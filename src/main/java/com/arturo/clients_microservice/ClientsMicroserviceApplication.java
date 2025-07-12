6d69917515bd70665e0f9eb07528ae70e1465fb7
titomon98 <arturomonterroso3@gmail.com>
2025-07-12 10:38:09 -0600

    Agregando commit inicial con proyecto base para trabajar test de empresa seek

---
+++ src/main/java/com/arturo/clients_microservice/ClientsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.arturo.clients_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturo/clients_microservice/ClientsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturo.clients_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
